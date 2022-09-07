5aa1c800b7e5c17b163efd85c4f620a17a74a7e4
oBarzilai <barzilai.yauh@gmail.com>
2022-09-07 11:34:43 -0300

    Criando primeiro componente

---
+++ frontend/src/components/NotificationButton/index.tsx
@@ -0,0 +1,13 @@
+import notificationButton from '../../assets/img/notification-icon.svg'
+
+import './styles.css'
+
+function NotificationButton() {
+    return (
+        <div className="barzilai-red-btn">
+            <img src={notificationButton} alt="Notificar" />
+        </div>
+    )
+}
+
+export default NotificationButton
