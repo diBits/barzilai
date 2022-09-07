45426b889b7f385ec3458f189b5ac317cbced24a
oBarzilai <barzilai.yauh@gmail.com>
2022-09-07 09:35:53 -0300

    Projeto Criado

---
+++ backend/src/main/java/com/yaudim/barzilai/BarzilaiApplication.java
@@ -0,0 +1,13 @@
+package com.yaudim.barzilai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarzilaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarzilaiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/yaudim/barzilai/BarzilaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yaudim.barzilai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarzilaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
