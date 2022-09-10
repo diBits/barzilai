d57c203132a7e53049dbcd938efeb67ddc80e012
oBarzilai <barzilai.yauh@gmail.com>
2022-09-09 23:38:04 -0300

    Enviar notificação

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,10 +1,27 @@
+import axios from 'axios';
 import notificationButton from '../../assets/img/notification-icon.svg';
+import { BASE_URL } from '../../utils/request';
 
 import './styles.css';
 
-function NotificationButton() {
+
+type Props = {
+
+    saleId : number;
+}
+
+function handleClick(id : number) {
+
+    axios(`${BASE_URL}/sales/${id}/notification`)
+        .then(response => {
+            console.log("SUCESSO");
+        })
+    
+}
+
+function NotificationButton({saleId} : Props) {
     return (
-        <div className="barzilai-red-btn">
+        <div className="barzilai-red-btn" onClick={() => handleClick(saleId)}>
             <img src={notificationButton} alt="Notificar" />
         </div>
     )
